b021410f8dcbab39b9b9ea5701a44ddc73591b28
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-04-27 17:29:50 -0300

    feat: Implementa a função get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,30 +5,19 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    dictionaryList = []
+    dictionary_list = []
     with open(path) as file:
         content = csv.DictReader(file)
         for line in content:
-            dictionaryList.append(line)
-        return dictionaryList
+            dictionary_list.append(line)
+        return dictionary_list
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs = read(path)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    job_types = set(job["job_type"] for job in jobs if job["job_type"])
+    return list(job_types)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
