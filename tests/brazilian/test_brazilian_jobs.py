d17c266863ffc88f99f37b95ae04f7648436fe13
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-04-28 15:04:50 -0300

    feat: Implementa um teste para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+expectedResult = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
 
 
 def test_brazilian_jobs():
-    pass
+    results = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert results[0] == expectedResult
